d2dfd64555b41f583170f9e69befe453ef7a64a7
Daniel Griffin <danielseangriffin@gmail.com>
2016-06-11 01:19:57 +0100

    remove debug

--- target/debug/freefare.d
+++
@@ -1,5 +0,0 @@
-/Users/dgriffin/Documents/projects/personal/rust/freefare/target/debug/libfreefare.rlib: src/lib.rs
-
-/Users/dgriffin/Documents/projects/personal/rust/freefare/target/debug/freefare.d: src/lib.rs
-
-src/lib.rs:
